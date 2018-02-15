e27938f11304576b12b787f24e5f4850e711d9a5
shahriaranwar <shahriaranwar@augmedix.com>
2018-02-16 00:43:27 +0600

    Takes screenshot of carbon.now.sh with provided code content

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 fs.readFile('./demoscripts/Main.java', {encoding: 'utf-8'}, (err, data) => {
     if (err) {
         throw err;
     }
 
-    console.log(`Normal data is:\n${data}`);
-    console.log(`Encoded data is:\n${encodeURIComponent(data)}`);
-    
+    const encodedContnt = encodeURIComponent(data);
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const carbonPrefix = 'https://carbon.now.sh/?bg=rgba(171,%20184,%20195,%201)&t=seti&l=auto&ds=true&wc=true&wa=true&pv=48px&ph=32px&ln=false&code=';
+        await page.goto(`${carbonPrefix + encodedContnt}`);
+        await page.screenshot({path: 'carbon.png'});
+      
+        await browser.close();
+    })();    
 });
