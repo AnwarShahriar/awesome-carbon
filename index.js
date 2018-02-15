bacf48c21bda701402b5b14abb970df065e9ecfd
shahriaranwar <shahriaranwar@augmedix.com>
2018-02-16 01:58:01 +0600

    Added ora as cli progress view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const ora = require('ora');
 const puppeteer = require('puppeteer');
 
 fs.readFile('./demoscripts/Main.java', {encoding: 'utf-8'}, (err, data) => {
@@ -9,6 +10,8 @@ fs.readFile('./demoscripts/Main.java', {encoding: 'utf-8'}, (err, data) => {
 
     const encodedContnt = encodeURIComponent(data);
     (async () => {
+        const spinner = ora('Preparing your source').start();
+
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page._client.send('Page.setDownloadBehavior', {
@@ -17,9 +20,14 @@ fs.readFile('./demoscripts/Main.java', {encoding: 'utf-8'}, (err, data) => {
         });
         const carbonPrefix = 'https://carbon.now.sh/?bg=rgba(171,%20184,%20195,%201)&t=seti&l=auto&ds=true&wc=true&wa=true&pv=48px&ph=32px&ln=false&code=';
         await page.goto(`${carbonPrefix + encodedContnt}`);
-        await page.click('#toolbar > div.jsx-2035980575.buttons > button:nth-child(2) > span');
         
+        spinner.succeed('Source is prepared');
+        spinner.start('Carboning you source');
+        
+        await page.click('#toolbar > div.jsx-2035980575.buttons > button:nth-child(2) > span');
         await page.waitForNavigation({waitUntil: 'networkidle0'});
         await browser.close();
+
+        spinner.succeed('Carbon is ready');
     })();    
 });
